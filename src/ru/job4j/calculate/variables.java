d89b39886b922e2bca519d4dee084d35cb614833
elya1993 <79111288718@yandex.ru>
2021-03-22 22:17:53 +0300

    calculate

---
+++ src/ru/job4j/calculate/variables.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+public class variables {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        float lenght = 78.0332F;
+        System.out.println(lenght);
+        boolean baby = true;
+        System.out.println(baby);
+
+
+    }
+}
