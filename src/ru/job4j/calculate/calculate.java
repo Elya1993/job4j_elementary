623476580aa166e479e76bb647521c6b29d33df9
elya1993 <79111288718@yandex.ru>
2021-03-22 23:26:18 +0300

    calculate

---
+++ src/ru/job4j/calculate/calculate.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+public class calculate {
+
+    public static void main(String[] args) {
+        int six = 6;
+        int two = 2;
+        int sixDivTwo = six / two;
+        System.out.println(sixDivTwo);
+
+
+        int four = 4;
+        int two = 2;
+        int fourTimeTwo = four * two;
+        System.out.println(fourTimeTwo);
+
+
+
+        int five = 5;
+        int two = 2;
+        int fiveMinusTwo = five - two;
+        System.out.println(fiveMinusTwo);
+
+    }
+}
