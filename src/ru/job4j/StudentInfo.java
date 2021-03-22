c62f544ef4acfbb900f5644a4e9a9e387805a44f
elya1993 <79111288718@yandex.ru>
2021-03-22 21:17:17 +0300

    Info

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;  // заголовок
+
+public class StudentInfo {  // имя файла
+    public static void main(String[] args) {
+        System.out.println("Умерова Эля");
+        System.out.println("26.03.1993");
+    }
+}
