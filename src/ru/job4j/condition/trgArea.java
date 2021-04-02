c3ebd1cd32b8e64d0516fe60a96d58936367b30c
elya1993 <79111288718@yandex.ru>
2021-04-02 16:01:10 +0300

    trgArea

---
+++ src/ru/job4j/condition/trgArea.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class trgArea {
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    public static void main(String[] args) {
+        double a = 2;
+        double b = 2;
+        double c = 2;
+        double area = trgArea.area(2, 2, 2);
+        System.out.println("Площадь треугольника равна" + area);
+    }
+}
