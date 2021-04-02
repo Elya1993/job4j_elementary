7ee0dd8952c3910dab72bd66e457a5119fabb613
elya1993 <79111288718@yandex.ru>
2021-04-02 15:19:07 +0300

    SqArea

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,15 +2,14 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, double k) {
-        double rsl = -1;
-        return rsl;
-    }
-    public static void main(String[] args) {
-        double k = 2;
-        double p = 6;
-        double h = p / (2 * (k + 1));
-        double l = h * k;
-        System.out.println("Площадь треугольника равна" + l * h);
+        return Math.pow(p / (2 * (k + 1)), 2) * k;
     }
 
+    public static void main(String[] args) {
+        double k = 2;
+        int p = 6;
+        double s = SqArea.square((int) p, k);
+        System.out.println("Площадь треугольника равна" + s);
     }
+
+}
