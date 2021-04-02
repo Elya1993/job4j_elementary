e3e66ab868ddda5a0ae9ac4c9414c95b9bc44338
elya1993 <79111288718@yandex.ru>
2021-04-02 19:08:01 +0300

    Greeting

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String idea = "I like Java.";
+        System.out.println(idea);
+
+        String idea2 = "But I am a newbie";
+        String separate = " + ";
+        System.out.println(idea + idea2);
+        int year = 2021;
+        System.out.println(idea + year);
+
+    }
+}
