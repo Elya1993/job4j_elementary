90ebcccd96cc451c1d37607b95d607f885c615d4
elya1993 <79111288718@yandex.ru>
2021-04-02 14:40:21 +0300

    SqArea

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -2,14 +2,11 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = -1;
-        return rsl;
+        double distance = Math.sqrt((Math.pow((x2) - (x1), 2)) + (Math.pow((y2) - (y1), 2)));
+        return distance;
     }
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
-        double result1 = Math.pow((2) - (0), 2);
-        double result2 = Math.pow((0) - (0), 2);
-        double distance = Math.sqrt(result1 + result2);
-       System.out.println(distance);
+       System.out.println(result);
     }
 }
