3e87424008fa8a91a8ecb414e3594372261c5a56
elya1993 <79111288718@yandex.ru>
2021-04-04 19:29:12 +0300

    Greeting

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -6,9 +6,7 @@ public class Greeting {
         String idea = "I like Java.";
         System.out.println(idea);
 
-        String idea2 = "But I am a newbie";
-        String separate = " + ";
-        System.out.println(idea + idea2);
+        System.out.println(idea + "But I am a newbie");
         int year = 2021;
         System.out.println(idea + year);
 
