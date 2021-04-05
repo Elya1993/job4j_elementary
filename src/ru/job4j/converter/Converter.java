e5c1ee70f067bd44665e5623adcef7065a69f04f
elya1993 <79111288718@yandex.ru>
2021-04-05 16:16:20 +0300

    Converter

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -21,28 +21,31 @@ public class Converter {
         return rsl;
     }
 
-
     public static void main(String[] args) {
         int in = 200;
         int expected = 2;
         int out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
+
         int in1 = 200;
-        int expected1 = 2;
+        int expected1 = 3;
         int out1 = Converter.rubleToDollar(in1);
         boolean passed1 = expected == out;
+
         int in2 = 200;
-        int expected2 = 1200;
+        int expected2 = 12000;
         int out2 = Converter.dollarToRuble(in2);
         boolean passed2 = expected == out;
+
         int in3 = 200;
-        int expected3 = 1200;
-        int out3 = Converter.dollarToRuble(in3);
+        int expected3 = 14000;
+        int out3 = Converter.euroToRuble(in3);
         boolean passed3 = expected == out;
+
         System.out.println("200 рублей это два евро:" + passed);
-        System.out.println("200 рублей это два доллара:" + passed);
-        System.out.println("200 долларов это 1200 рублей:" + passed);
-        System.out.println("200 евро это 1400 рублей:" + passed);
+        System.out.println("200 рублей это три доллара:" + passed1);
+        System.out.println("200 долларов это 12000 рублей:" + passed2);
+        System.out.println("200 евро это 14000 рублей:" + passed3);
     }
 }
 
