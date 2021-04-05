71a219fa2aeb01bf67c9455858e55ec62ffea454
elya1993 <79111288718@yandex.ru>
2021-04-05 16:17:09 +0300

    ConverterTest.java

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -13,18 +13,29 @@ public class ConverterTest {
         int expected = 2;
         int out = Converter.rubleToEuro(in);
         Assert.assertEquals(expected, out);
-
     }
 
     @Test
-    public void rubleToDollar() {
+    public void whenConvert200RubleTo2Dollar() {
+        int in = 200;
+        int expected = 3;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
     }
 
     @Test
-    public void dollarToRuble() {
+    public void whenConvert200DollarTo12000Ruble() {
+        int in = 200;
+        int expected = 12000;
+        int out = Converter.dollarToRuble(in);
+        Assert.assertEquals(expected, out);
     }
 
     @Test
-    public void euroToRuble() {
+    public void whenConvert200EuroTo14000Ruble() {
+        int in = 200;
+        int expected = 14000;
+        int out = Converter.euroToRuble(in);
+        Assert.assertEquals(expected, out);
     }
 }
