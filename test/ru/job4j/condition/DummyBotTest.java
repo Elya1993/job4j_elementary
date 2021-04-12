c62edfb64c5ab7661e638c88e3d326cdcb61feda
elya1993 <79111288718@yandex.ru>
2021-04-12 21:35:36 +0300

    DummyBotTest.java

--- test/ru/job4j/condition/DummyBotTest.java
+++ test/ru/job4j/condition/DummyBotTest.java
@@ -5,35 +5,30 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class DummyBotTest { // Все равно выдает
-    // "Expected :Это ставит меня в тупик. Задайте другой вопрос
-    //  Actual   :Как дела?
-    //  Process finished with exit code -1"
+public class DummyBotTest {
 
     @Test
     public void whenAnswerPrivetBotThenPrivet() {
-        String in = "Привет, Бот!";
+        String in = "Привет, Бот.";
         String out = DummyBot.answer(in);
-        String expected = "Привет, умник!";
+        String expected = "Привет, умник.";
         Assert.assertEquals(expected, out);
     }
 
 
     @Test
     public void whenPoka() {
-        String in = "Пока!";
+        String in = "Пока.";
         String result = DummyBot.answer(in);
-        String expected = "До скорой встречи!";
+        String expected = "До скорой встречи.";
         Assert.assertEquals(expected, result);
     }
 
     @Test
     public void whenUnknownBot() {
-        String in = "Как дела?";
+        String in = "Как дела.";
         String out = DummyBot.answer(in);
-        String expected = "Это ставит меня в тупик. Задайте другой вопрос";
+        String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
         Assert.assertEquals(expected, out);
-
     }
-
 }
