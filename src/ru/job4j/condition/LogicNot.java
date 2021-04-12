d204b318fdad0789f295d298fd0da376e90cafe1
elya1993 <79111288718@yandex.ru>
2021-04-13 01:11:24 +0300

    LogicNot.java

--- src/ru/job4j/condition/LogicNot.java
+++ src/ru/job4j/condition/LogicNot.java
@@ -2,22 +2,21 @@ package ru.job4j.condition;
 
 public class LogicNot {
     public static boolean notPositive(int num) {
-        return (!(num > 0));
+        return !isPositive(num);
     }
 
     public static boolean notEvenAndPositive(int num) {
-        return (!(num % 2 == 0) && (num > 0));
+        return !isEven(num) && isPositive(num);
 
     }
 
     public static boolean evenOrNotPositive(int num) {
-        return ((num % 2 == 0) || !(num > 0));
+        return isEven(num) || !isPositive(num);
+
     }
 
     public static void main(String[] args) {
-        LogicNot.notPositive(5);
-        LogicNot.notEvenAndPositive(-5);
-        LogicNot.evenOrNotPositive(6);
+        LogicNot.notEvenAndPositive(4);
 
     }
 
