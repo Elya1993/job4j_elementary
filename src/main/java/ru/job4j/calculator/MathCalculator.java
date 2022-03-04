7152b1e498c11002bc3348d0a24a11391e0163cd
elya1993 <79111288718@yandex.ru>
2022-03-04 20:47:37 +0300

    import [#504773 #270432] MathCalculator

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,13 +3,24 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
-    public static double sumRsl1AndRsl2(double first, double  second) {
-        return (first - second) + (first / second); // (first - second) + (first / second) можно записать как rsl1 + rsl2?
-        //  class MathFunction я импортировала в данный класс, но смысл его импортировать, если значения не переносятся
+    public static double sumOfMinusAndDiv(double first, double second) {
+        double sum2 = (first - second) + (first / second);
+        return sum2;
+    }
+
+    public static double finish(double first, double second) {
+        double finish = ((first - second) + (first / second) + ((first - second) + (first / second)));
+        //почему вместо "(first - second)" я не могу написать просто переменную "minus1", которая в классе MathFunction
+        //принимает значение "double minus1 = first - second"?
+        //пример: double finish = minus1 + div1 + sum2
+
+        return finish;
+
     }
 
     public static void main(String[] args) {
-        System.out.println(sumRsl1AndRsl2(10, 20));
+        System.out.println(MathCalculator.sumOfMinusAndDiv(10, 20));
+        System.out.println(MathCalculator.finish(10, 20));
+
     }
 }
-// в задании указано вычислить сумму четрыех операций, а их получается три
