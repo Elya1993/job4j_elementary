ab3b80a872d7c5a5fdb82d78e9e028ce3df8f7d6
elya1993 <79111288718@yandex.ru>
2022-03-11 21:32:37 +0300

    Triangle.java

--- src/main/java/ru/job4j/condition/Triangle.java
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -1,14 +1,8 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    //Нужно проверить, что ab + ac > bc и ac + bc > ab и ab + bc > ac - в этом случае треугольник существует.
-    //В этом задании нужно использовать булеву логику и оператор && (логическое и).
     public static boolean exist(double ab, double bc, double ac) {
-        if (ab + ac > bc && ab + bc > ac && ac + bc > ab) {
-            return true;
-        } else {
-            return false;
-        }
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
     }
 
     public static void main(String[] args) {
