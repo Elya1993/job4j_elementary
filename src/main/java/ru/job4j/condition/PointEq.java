ba19aa50738f0f992308acd750fca76b44a45dbd
elya1993 <79111288718@yandex.ru>
2022-03-11 20:18:05 +0300

    PointEq.java

--- src/main/java/ru/job4j/condition/PointEq.java
+++ src/main/java/ru/job4j/condition/PointEq.java
@@ -2,17 +2,13 @@ package ru.job4j.condition;
 
 public class PointEq { // 1. Ниже приведен код. Метод eq проверяет, что две точки в системе координат одинаковы.
     public static boolean eq(int x1, int y1, int x2, int y2) {
-        if (x1 == x2 && y1 == y2) {
-            return true;
-        } else {
-            return false;
-        }
+        return x1 == x2 && y1 == y2;
     }
 
 
-    public static void main (String[]args){
+    public static void main(String[] args) {
         PointEq.eq(1, 1, 2, 2);
         PointEq.eq(10, 10, 10, 10);
     }
 }
-                      // return должен вывести True или False?
+// return должен вывести True или False?
